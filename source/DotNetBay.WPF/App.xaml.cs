31884853cae53a7c6e7fd8f029bad999afb20c3b
wembley-fraggle <roman.holzner@students.fhnw.ch>
2015-02-27 21:32:16 -0800

    Lab02: 1.4, Application Startup, DemoDaten Added Initial Demo Data to tehe service.

--- source/DotNetBay.WPF/App.xaml.cs
+++ source/DotNetBay.WPF/App.xaml.cs
@@ -6,10 +6,12 @@ using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using DotNetBay.Core;
 using DotNetBay.Interfaces;
 using DotNetBay.Data;
 using DotNetBay.Data.FileStorage;
 using DotNetBay.Core.Execution;
+using DotNetBay.Model;
 
 namespace DotNetBay.WPF
 {
@@ -21,9 +23,10 @@ namespace DotNetBay.WPF
         
         public App()
         {
-            this.MainRepository = new FileSystemRepositoryFactory("test.txt").CreateMainRepository();
+            this.MainRepository = new FileSystemRepositoryFactory("store.json").CreateMainRepository();
             this.AuctionRunner = new AuctionRunner(this.MainRepository);
             this.AuctionRunner.Start();
+            this.InitDemoAuctions();
         }
 
         public IMainRepository MainRepository
@@ -34,6 +37,24 @@ namespace DotNetBay.WPF
         public IAuctionRunner AuctionRunner
         {
             get; private set;
+        }
+
+        private void InitDemoAuctions()
+        {
+            var memberService = new SimpleMemberService(this.MainRepository);
+            var service = new AuctionService(this.MainRepository, memberService);
+            if (!service.GetAll().Any())
+            {
+                var me = memberService.GetCurrentMember();
+                service.Save(new Auction
+                {
+                    Title = "My First Auction",
+                    StartDateTimeUtc = DateTime.UtcNow.AddSeconds(10),
+                    EndDateTimeUtc = DateTime.UtcNow.AddDays(14),
+                    StartPrice = 72,
+                    Seller = me
+                });
+            }
         }
     }
 }
