c2fa5cfa16394d92aeff38d7dedb017f7eca623d
wembley-fraggle <roman.holzner@students.fhnw.ch>
2015-02-27 21:11:00 -0800

    Lab02: Aufgabe 1.4 Application Setup

--- source/DotNetBay.WPF/App.xaml.cs
+++ source/DotNetBay.WPF/App.xaml.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using DotNetBay.Interfaces;
+using DotNetBay.Data;
+using DotNetBay.Data.FileStorage;
+using DotNetBay.Core.Execution;
 
 namespace DotNetBay.WPF
 {
@@ -13,5 +18,22 @@ namespace DotNetBay.WPF
     /// </summary>
     public partial class App : Application
     {
+        
+        public App()
+        {
+            this.MainRepository = new FileSystemRepositoryFactory("test.txt").CreateMainRepository();
+            this.AuctionRunner = new AuctionRunner(this.MainRepository);
+            this.AuctionRunner.Start();
+        }
+
+        public IMainRepository MainRepository
+        {
+            get; private set;
+        }
+
+        public IAuctionRunner AuctionRunner
+        {
+            get; private set;
+        }
     }
 }
