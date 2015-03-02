381b5b32705e0e52851c8b0b76c5dfc8ef193f15
Tizian Schmidlin <tizian.schmidlin@students.fhnw.ch>
2015-03-02 10:35:30 +0100

    Removed BidCmd Refactored Commands to use only AuctionCommand<T> Added CloseWindowAction to cancel buttons

---
+++ source/DotNetBay.WPF/Command/AuctionCmd.cs
@@ -0,0 +1,30 @@
+﻿using DotNetBay.WPF.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DotNetBay.WPF.Command
+{
+    public sealed class AuctionCmd<T> : ICommand
+    {
+        public Action<T> ExecuteDelegate { get; set; }
+        public Func<T, bool> CanExecuteDelegate { get; set; }
+            
+        #region ICommand Members
+
+        public bool CanExecute(object parameter) {
+            return CanExecuteDelegate((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter) {
+            ExecuteDelegate((T)parameter);
+        }
+            
+        #endregion
+    }
+}
--- source/DotNetBay.WPF/ViewModel/ViewModelBase.cs
+++ source/DotNetBay.WPF/ViewModel/ViewModelBase.cs
@@ -4,11 +4,30 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using DotNetBay.WPF.Command;
 
 namespace DotNetBay.WPF.ViewModel
 {
     class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public AuctionCmd<Window> CloseWindowCommand { get; private set; }
+
+        public ViewModelBase()
+        {
+            this.CloseWindowCommand = new AuctionCmd<Window>();
+            this.CloseWindowCommand.ExecuteDelegate = this.CloseWindow;
+            this.CloseWindowCommand.CanExecuteDelegate = (o) => true; 
+        }
+
+        private void CloseWindow(Window window)
+        {
+            if (window != null)
+            {
+                window.Close();
+            }
+        }
     }
 }
