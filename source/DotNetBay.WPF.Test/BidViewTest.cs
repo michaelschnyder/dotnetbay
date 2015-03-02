ba8f05325867a73dec743b7ca30047f8cfde9aa8
Tizian Schmidlin <tizian.schmidlin@students.fhnw.ch>
2015-03-02 10:47:02 +0100

    Added Unit Tests

---
+++ source/DotNetBay.WPF.Test/BidViewTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetBay.WPF.Test
+{
+    [TestClass]
+    public class BidViewTest
+    {
+        [TestMethod]
+        public void TestPlaceBid()
+        {
+        }
+    }
+}
---
+++ source/DotNetBay.WPF.Test/SellViewtest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetBay.WPF.Test
+{
+    [TestClass]
+    public class SellViewTest
+    {
+        [TestMethod]
+        public void TestPlaceSell()
+        {
+        }
+    }
+}
