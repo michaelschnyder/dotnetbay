412e4ea3e1a2a0dbd2de9d0968cd7b808a44ba32
Tizian Schmidlin <tizian.schmidlin@students.fhnw.ch>
2015-03-02 09:38:56 +0100

    Added datacontext binding to View constructors

--- source/DotNetBay.WPF/View/BidView.xaml.cs
+++ source/DotNetBay.WPF/View/BidView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotNetBay.WPF.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace DotNetBay.WPF.View
         public BidView()
         {
             InitializeComponent();
+            this.DataContext = new BidViewModel();
         }
     }
 }
--- source/DotNetBay.WPF/View/SellView.xaml.cs
+++ source/DotNetBay.WPF/View/SellView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotNetBay.WPF.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace DotNetBay.WPF
         public SellView()
         {
             InitializeComponent();
+            this.DataContext = new SellViewModel();
         }
     }
 }
