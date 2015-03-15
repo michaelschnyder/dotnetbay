9b456c00a961edc825e701d51ef74d71a99bf653
wembley-fraggle <roman.holzner@students.fhnw.ch>
2015-03-15 03:55:32 -0700

    added new EF Repositorz Factory

---
+++ source/DotNetBay.Data.EF/EFMainRepositoryFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetBay.Interfaces;
+
+namespace DotNetBay.Data.EF
+{
+    class EFMainRepositoryFactory : IRepositoryFactory
+    {
+        public void Dispose()
+        {
+        }
+
+        public IMainRepository CreateMainRepository()
+        {
+            return new EFMainRepository();
+        }
+    }
+}
