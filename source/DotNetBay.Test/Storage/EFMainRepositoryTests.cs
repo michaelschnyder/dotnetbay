a836f96c80368cca00cae1e32913bcaafb3fea3e
wembley-fraggle <roman.holzner@students.fhnw.ch>
2015-03-15 09:50:09 -0700

    added category for EF Test (in order to Ignore this in App-Veyor

--- source/DotNetBay.Test/Storage/EFMainRepositoryTests.cs
+++ source/DotNetBay.Test/Storage/EFMainRepositoryTests.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DotNetBay.Interfaces;
 using DotNetBay.Data.EF;
+using NUnit.Framework;
 
 namespace DotNetBay.Test.Storage
 {
+    [Category("Database"),Ignore]
     class EFMainRepositoryTests : MainRepositoryTestBase
     {
         protected override IRepositoryFactory CreateFactory()
         {
+            var ensureDLLIsCopied = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
             return new EFMainRepositoryFactory();
         }
     }
