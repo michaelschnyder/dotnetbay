6d971b541d4dcfe515c49da649da977805d01204
wembley-fraggle <roman.holzner@students.fhnw.ch>
2015-03-15 08:45:39 -0700

    Entity Relations

--- source/DotNetBay.Data.EF/MainDbContext.cs
+++ source/DotNetBay.Data.EF/MainDbContext.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
 using DotNetBay.Model;
+using System.ComponentModel.DataAnnotations;
 
 namespace DotNetBay.Data.EF
 {
@@ -15,6 +16,22 @@ namespace DotNetBay.Data.EF
         {
             this.Configuration.ProxyCreationEnabled = false;
             this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.AutoDetectChangesEnabled = true; // FIXME
+        }
+
+        public DbSet<Auction> Auctions { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+        public DbSet<Member> Members { get; set; }
+        
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Member>().HasKey(m => m.UniqueId);
+
+            modelBuilder.Entity<Auction>().HasRequired(m => m.Seller).WithMany(m => m.Auctions);
+            modelBuilder.Entity<Bid>().HasRequired(a => a.Auction).WithMany(m => m.Bids);
+
+
         }
 
     }
