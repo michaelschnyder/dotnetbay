b64dd44659f1d7f2be281e0a329818d83b7e4642
wembley-fraggle <roman.holzner@students.fhnw.ch>
2015-03-15 03:43:53 -0700

    added method implementations

--- source/DotNetBay.Data.EF/EFMainRepository.cs
+++ source/DotNetBay.Data.EF/EFMainRepository.cs
@@ -4,10 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DotNetBay.Interfaces;
+using DotNetBay.Model;
 
 namespace DotNetBay.Data.EF
 {
     class EFMainRepository : IMainRepository
     {
+        public EFMainRepository()
+        {
+        }
+
+        public IQueryable<Auction> GetAuctions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Member> GetMembers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Auction Add(Auction auction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Auction Update(Auction auction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bid Add(Bid bid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bid GetBidByTransactionId(Guid transactionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member Add(Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
