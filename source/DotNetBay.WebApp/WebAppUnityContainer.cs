d22760d24eff64fe466f91259c0d3feca2961c38
Michael Schnyder <michael.schnyder@zuehlke.com>
2015-05-18 19:16:44 +0200

    added Unity Container

---
+++ source/DotNetBay.WebApp/WebAppUnityContainer.cs
@@ -0,0 +1,33 @@
+﻿using DotNetBay.Core;
+using DotNetBay.Data.EF;
+using DotNetBay.Interfaces;
+
+using Microsoft.Practices.Unity;
+
+namespace DotNetBay.WebApp
+{
+    public class WebAppUnityContainer : UnityContainer
+    {
+        private static WebAppUnityContainer instance;
+
+        public WebAppUnityContainer()
+        {
+            this.RegisterType<IMainRepository, EFMainRepository>(new HierarchicalLifetimeManager());
+            this.RegisterType<IAuctionService, AuctionService>(new HierarchicalLifetimeManager());
+            this.RegisterType<IMemberService, SimpleMemberService>(new HierarchicalLifetimeManager());
+        }
+
+        public static IUnityContainer Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new WebAppUnityContainer();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
